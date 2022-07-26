7a728b6de9d4d22961566ddf5497987d6ac77180
kilgur85 <103890819+kilgur85@users.noreply.github.com>
2022-07-26 11:06:27 +0300

    Изменил BubleSort на SelectionSort в Main

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int[] array = {1,15,22,68,2,44,3,50};
 Sorter sorter = new Sorter();
-sorter.setStrategy(new BubleSort());
+sorter.setStrategy(new SelectionsSort());
 sorter.executesort(array);
 System.out.println(Arrays.toString(array));
 
